bc77a120b83b070a53a7a5a6ca8e9e34c8198468
JohnMwaniki-TheJitu <john.mwaniki@griffinglobaltech.com>
2024-08-31 17:04:17 +0300

    feat: add login-register routing

--- eventz-ui/src/app/app.routes.ts
+++ eventz-ui/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { LoginComponent } from '../pages/login/login.component';
+import path from 'node:path';
+import { RegisterComponent } from '../pages/register/register.component';
+export const routes: Routes = [
+   {path:'', component: RegisterComponent},
+   {path:'login', component: LoginComponent}
+];
