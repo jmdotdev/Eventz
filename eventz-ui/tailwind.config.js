009559ad65bccd7d273d7a55c78d58a77e5ce785
pochii-cloud <johnmwanikig2000@gmail.com>
2024-09-15 13:36:34 +0300

    feat: add landing page

--- eventz-ui/tailwind.config.js
+++ eventz-ui/tailwind.config.js
@@ -7,10 +7,13 @@ module.exports = {
     extend: {
       colors:{
         deepBlue: '#381ddb',
-        navBarBackground: '#08012B'
+        navBarBackground: '#08012B',
+        paleOrange: '#FFA3A3'
+      },
+      boxShadow: {
+        'customWhite': 'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset',
       }
     },
   },
   plugins: [],
 }
-
