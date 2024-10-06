e33bbf266ab74027d05ba3941b88dafd0424fc9c
pochii-cloud <johnmwanikig2000@gmail.com>
2024-10-06 22:01:01 +0300

    feat: add footer component

--- eventz-ui/tailwind.config.js
+++ eventz-ui/tailwind.config.js
@@ -14,7 +14,8 @@ module.exports = {
         eventsFilters: '#F2F4FF',
         lightPurple: '#EFECFC',
         paleWhite: '#F8F8F8',
-        palegray: '#E0E0E0'
+        palegray: '#E0E0E0',
+        footerBackground:'#0A075F'
       },
       boxShadow: {
         'customWhite': 'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset',
