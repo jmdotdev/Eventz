e50052804f5803e7064366805c7d411cdb875cdf
JohnMwaniki-TheJitu <john.mwaniki@griffinglobaltech.com>
2024-10-06 14:33:18 +0300

    feat: add contact form component

--- eventz-ui/tailwind.config.js
+++ eventz-ui/tailwind.config.js
@@ -12,7 +12,9 @@ module.exports = {
         eventCardBackground: '#F3FAFF',
         eventsTitle: '#242565',
         eventsFilters: '#F2F4FF',
-        lightPurple: '#EFECFC'
+        lightPurple: '#EFECFC',
+        paleWhite: '#F8F8F8',
+        palegray: '#E0E0E0'
       },
       boxShadow: {
         'customWhite': 'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset',
