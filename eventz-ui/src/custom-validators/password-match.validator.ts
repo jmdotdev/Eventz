6c2225915cadb92b3a8493aa3421dce7182d3640
JohnMwaniki-TheJitu <john.mwaniki@griffinglobaltech.com>
2024-10-18 15:44:59 +0300

    feat: add register logic

---
+++ eventz-ui/src/custom-validators/password-match.validator.ts
@@ -0,0 +1,24 @@
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+
+export function passwordMatchValidator(controlName: string, matchingControlName: string): ValidatorFn {
+  return (formGroup: AbstractControl): { [key: string]: boolean } | null => {
+    const passwordControl = formGroup.get(controlName);
+    const confirmPasswordControl = formGroup.get(matchingControlName);
+
+    if (!passwordControl || !confirmPasswordControl) {
+      return null;
+    }
+
+    if (confirmPasswordControl.errors && !confirmPasswordControl.errors['passwordMismatch']) {
+      return null;
+    }
+
+    if (passwordControl.value !== confirmPasswordControl.value) {
+      confirmPasswordControl.setErrors({ passwordMismatch: true });
+    } else {
+      confirmPasswordControl.setErrors(null);
+    }
+
+    return null;
+  };
+}
