2ab2734500e8e4ff50e56c2a8dd56d29a855bd3f
pochii-cloud <johnmwanikig2000@gmail.com>
2024-10-29 22:57:21 +0300

    checkout pae

--- eventz-ui/src/app/app.routes.ts
+++ eventz-ui/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LoginComponent } from '../pages/login/login.component';
 import { LandingComponent } from '../pages/landing/landing.component';
 import { RegisterComponent } from '../pages/register/register.component';
 import { EventDetailComponent } from '../pages/event-detail/event-detail.component';
+import { CheckoutComponent } from '../pages/checkout/checkout.component';
 export const routes: Routes = [
    {path:'',component: LandingComponent},
    {path:'login', component: LoginComponent},
    {path:'register', component: RegisterComponent},
-   {path:'event-detail/:id',component: EventDetailComponent}
+   {path:'event-detail/:id',component: EventDetailComponent},
+   {path:'checkout',component: CheckoutComponent}
 ];
