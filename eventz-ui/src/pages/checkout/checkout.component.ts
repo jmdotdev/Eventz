bab468dd5c4d648d0d13836ef32b713408d1d026
JohnMwaniki-TheJitu <john.mwaniki@griffinglobaltech.com>
2024-11-06 17:52:54 +0300

    feat: finish checkout page and add responsiveness

--- eventz-ui/src/pages/checkout/checkout.component.ts
+++ eventz-ui/src/pages/checkout/checkout.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
-
+import { faStopwatch } from '@fortawesome/free-solid-svg-icons';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FooterComponent } from "../../components/footer/footer.component";
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FontAwesomeModule, FooterComponent],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss'
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
+  watch = faStopwatch;
+  location = faMapMarkerAlt;
+  ngOnInit(): void {
+    
+  }
 
 }
