4db9815946a2aa9640fba0154a2e05478deb71df
jmdotdev <johnmwanikig2000@gmail.com>
2025-05-14 00:15:47 +0300

    finish admin layout implementation

--- eventz-ui/src/layouts/admin-layout/admin-layout.component.ts
+++ eventz-ui/src/layouts/admin-layout/admin-layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SideNavComponent } from "../../components/admin/side-nav/side-nav.component";
+import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from "../../components/admin/top-bar/top-bar.component";
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [SideNavComponent],
+  imports: [SideNavComponent, RouterOutlet, TopBarComponent],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss'
 })
