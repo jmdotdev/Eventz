d89b56fc8b70edad7fa751a780c0267e346fcbee
JohnMwaniki <johnmwanikig2000@gmail.com>
2025-12-27 23:53:10 +0300

    feat: add categories  handlers and commands

---
+++ eventz-api/eventz.application/Features/Categories/CreateCategory/CreateCategoryHandler.cs
@@ -0,0 +1,28 @@
+﻿using Eventz.Application.Interfaces;
+using Eventz.Domain.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventz.Application.Features.Categories.CreateCategory
+{
+    public class CreateCategoryHandler
+    {
+        private readonly ICategoryRepository _repository;
+        public CreateCategoryHandler(ICategoryRepository repository) {
+             _repository = repository;
+        }
+
+        public async Task<Category> Handle (CreateCategoryCommand createCategoryCommand, CancellationToken cancellationToken)
+        {
+            var category = new Category {
+                Name = createCategoryCommand.Name,
+                Description = createCategoryCommand.Description
+            };
+            await _repository.CreateCategory(category);
+            return category;
+        }
+    }
+}
---
+++ eventz-api/eventz.infrastructure/Repositories/Categories/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using Eventz.Application.Interfaces;
+using Eventz.Domain.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventz.Infrastructure.Repositories.Categories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _context;
+
+        private CategoryRepository (AppDbContext context)
+        {
+            _context = context;
+        }
+      
+        public async Task CreateCategory(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
