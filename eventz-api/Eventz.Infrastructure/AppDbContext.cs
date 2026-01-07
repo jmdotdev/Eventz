cc6098feec782f8114b2be2777f0ba9b8aee8df9
JohnMwaniki <johnmwanikig2000@gmail.com>
2026-01-07 12:47:18 +0300

    chore: rename folder/csproj files to follow correct convention

---
+++ eventz-api/Eventz.Infrastructure/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Eventz.Domain.Entitites;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eventz.Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Event> Events => Set<Event>();
+
+        public DbSet<Category> Categories => Set<Category>();
+    }
+}
---
+++ eventz-api/Eventz.Infrastructure/Repositories/Categories/CategoryRepository.cs
@@ -0,0 +1,39 @@
+﻿using Eventz.Application.Dtos;
+using Eventz.Application.Interfaces;
+using Eventz.Domain.Entitites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventz.Infrastructure.Repositories.Categories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryRepository (AppDbContext context)
+        {
+            _context = context;
+        }
+      
+        public async Task CreateCategory(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<CategoryDto>> GetCategories ()
+        {
+           var result = await _context.Categories.Select(c => new CategoryDto
+           {
+               CategoryToken = c.CategoryToken,
+               Name = c.Name,
+               Description = c.Description
+           }).ToListAsync();
+           return result;
+        }
+    }
+}
