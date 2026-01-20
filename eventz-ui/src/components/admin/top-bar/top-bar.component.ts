1aa0f945fd186906a0572d99aaf70c48b85af205
JohnMwaniki <johnmwanikig2000@gmail.com>
2026-01-21 00:07:26 +0300

    feat: admin sidebar responsiveness

--- eventz-ui/src/components/admin/top-bar/top-bar.component.ts
+++ eventz-ui/src/components/admin/top-bar/top-bar.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
-import { faBell, faDotCircle } from '@fortawesome/free-solid-svg-icons';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { faBell, faDotCircle, faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss'
 })
 export class TopBarComponent {
     faBell = faBell;
     faDotCircle = faDotCircle;
+    faBars = faBars;
+    @Output() menuClick = new EventEmitter<void>();
 }
