283ae76e48f9ed241ff37346f78654759752a76c
JohnMwaniki <johnmwanikig2000@gmail.com>
2026-01-21 00:07:26 +0300

    feat: admin sidebar responsiveness

--- eventz-ui/src/layouts/admin-layout/admin-layout.component.ts
+++ eventz-ui/src/layouts/admin-layout/admin-layout.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { SideNavComponent } from "../../components/admin/side-nav/side-nav.component";
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from "../../components/admin/top-bar/top-bar.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [SideNavComponent, RouterOutlet, TopBarComponent],
+  imports: [CommonModule, SideNavComponent, RouterOutlet, TopBarComponent],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss'
 })
 export class AdminLayoutComponent {
+showSideNav = false;
+
+toggleSideNav() {
+  this.showSideNav = !this.showSideNav;
+}
 
 }
